e70d35320ad247758edf3fddf88d95ccdf69b983
bush <56284466+realbush@users.noreply.github.com>
2023-03-06 00:02:43 +0800

    Update wzjs.js

--- wzjs.js
+++ wzjs.js
@@ -21,3 +21,29 @@ function searchs() {
         d.type = "hidden";
     }
 }
+function shijian() {
+    var date = new Date();
+    var now = "";
+    if (date.getHours() < 10) {
+        hours = "0" + date.getHours();
+    }
+    else {
+        hours = date.getHours();
+    }
+    if (date.getMinutes() < 10) {
+        minutes = "0" + date.getMinutes();
+    }
+    else {
+        minutes = date.getMinutes();
+    }
+    if (date.getSeconds() < 10) {
+        seconds = "0" + date.getSeconds();
+    }
+    else {
+        seconds = date.getSeconds();
+    }
+    now = now + hours + ":" + minutes + ":" + seconds;
+
+    document.getElementById("time").innerHTML = now;
+    setTimeout("shijian()", 1000);
+}
